f45d9069c73a1f8bbcc8221ea0e6cb466c6e4f4e
nico <nicolaslehir@gmail.com>
2022-09-29 20:07:30 +0200

    add complexities

---
+++ code/complexities/insertion_sort/insertion_sort.py
@@ -0,0 +1,49 @@
+def verboseprint(var, verbose):
+    """
+        For the next exercice it will be more convenient
+        to have less output printed, so we use this function.
+    """
+    if verbose:
+        print(var)
+
+
+def insertionSort(llist: list, verbose: bool=True) -> int:
+    """
+        In Python you can modify objects defined
+        outside the scope of a function
+        if you use methods from their class.
+
+        our function will return the number of
+        elementary operations performed.
+    """
+    verboseprint(f"initial list:\n{llist}", verbose)
+    counter = 0
+    for i in range(1, len(llist)):
+        counter += 1
+        verboseprint("\n--", verbose)
+        verboseprint(f"position : {i}", verbose)
+
+        temp = llist[i]
+        verboseprint(f"temp : {temp}", verbose)
+
+        # Move elements of llist[0..i-1], that are
+        # greater than temp, to one position ahead
+        # of their current position
+        j = i-1
+        """
+        EDIT THIS LOOP
+        """
+        while j >= 0 and True:
+            counter += 1
+            verboseprint(f"move {llist[j]} to the right", verbose)
+            llist[-1] = 3
+            j = j-1
+        verboseprint(f"place {temp} in position {j+1}", verbose)
+        llist[j + 1] = temp
+        verboseprint(f"new list:\n{llist}", verbose)
+    verboseprint(f"{counter} operations performed", verbose)
+    return counter
+
+
+llist = [10, 2, 11, 17, 5, 3, 12, 1]
+insertionSort(llist)
---
+++ code/complexities/matrix_multiplication.py
@@ -0,0 +1,29 @@
+"""
+    measure the complexity of matrix multiplication
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from time import time
+
+matrix_sizes = range(0, 5000, 500)
+times = list()
+
+for n in matrix_sizes:
+    # generate random matrices
+    a = np.random.rand(n, n)
+    b = np.random.rand(n, n)
+    t0 = time()
+    c = np.matmul(a, b)
+    computation_time = time() - t0
+    times.append(computation_time)
+    print(n, computation_time)
+
+title = "Computation time of matrix multiplication"
+filename = "matrix_multiplication.pdf"
+plt.plot(matrix_sizes, times, "o")
+plt.xlabel("matrix size")
+plt.ylabel("computation time")
+plt.title(title)
+plt.savefig("images/" + filename)
+plt.close()
