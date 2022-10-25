611cd7f0703064503c9ba50b4b5b38b0d7ea9b82
nico <nicolaslehir@gmail.com>
2022-10-25 20:48:23 +0200

    fix project deadlines

--- code/recursion/bad_recursion.py
+++ code/recursion/bad_recursion.py
@@ -1,8 +1,14 @@
-def badrec(x):
+import sys
+sys.setrecursionlimit(int(1e5))
+
+
+def badrec(x, depth):
     if x > 0:
-        return badrec(x / 2)
+        print(x/2)
+        print(depth)
+        return badrec(x / 2, depth + 1)
     else:
         return 1
 
 
-print(badrec(3))
+print(badrec(3, 0))
