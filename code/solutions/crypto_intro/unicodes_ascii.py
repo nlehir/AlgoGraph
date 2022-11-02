af363453101c0c0736db4319c267eab2b4176192
nico <nicolaslehir@gmail.com>
2022-11-02 22:10:32 +0100

    update course 221102

--- code/solutions/crypto_intro/unicodes_ascii.py
+++
@@ -1,2 +0,0 @@
-for code in range(55, 100):
-    print(f"code: {code} --> character: {chr(code)}")
--- code/solutions/graphs/dominating_set/read_params.py
+++
@@ -1,7 +0,0 @@
-def read_params() -> list:
-    params = list()
-    f = open("./params.txt", "r")
-    for line in f:
-        param = int(line.split("=")[1])
-        params.append(param)
-    return params
