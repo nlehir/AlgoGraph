5a31110fc8bc15b36f110372241e9893230da461
Nicolas Le Hir <nicolaslehir@gmail.com>
2023-09-26 22:24:17 +0200

    remove solutions

--- code/solutions/graphs/dominating_set/dominating_nx.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-from networkx.algorithms.dominating import dominating_set, is_dominating_set
-
-G = nx.generators.random_graphs.gnm_random_graph(20, 80)
-D = dominating_set(G)
-
-print(f"Dominating set: {D}")
-print(is_dominating_set(G, D))
