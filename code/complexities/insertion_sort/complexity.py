8755ce30a40bd4334bb414a149017079d096a6cd
Nicolas Le Hir <nicolaslehir@gmail.com>
2023-10-13 12:55:47 +0200

    update insertion sort

--- code/complexities/insertion_sort/complexity.py
+++ code/complexities/insertion_sort/complexity.py
@@ -38,33 +38,36 @@ def number_of_operations(n: int) -> float:
     )
     return average
 
+def main() -> None:
+    """
+        Compute the average number of operations
+        for several values of n
+    """
+    n_max = 200
+    step = int(n_max / 10)
+    n_range = range(1, n_max, step)
+    experimental_values = [number_of_operations(n) for n in n_range]
+    plt.plot(n_range, experimental_values, "o", color="teal", label="experiment")
 
-"""
-    Compute the average number of operations
-    for several values of n
-"""
-n_max = 200
-step = int(n_max / 10)
-n_range = range(1, n_max, step)
-experimental_values = [number_of_operations(n) for n in n_range]
-plt.plot(n_range, experimental_values, "o", color="teal", label="experiment")
+    """
+        Compute theoretical values
+    """
+    """
+    EDIT
+    """
+    values_of_n = range(1, n_max)
+    theoretical_values = [2**n for n in values_of_n]
 
-"""
-    Compute theoretical values
-"""
-"""
-EDIT
-"""
-values_of_n = range(1, n_max)
-theoretical_values = [2**n for n in values_of_n]
+    """
+        Plot the results.
+    """
+    plt.xticks(range(0, n_max, step))
+    plt.plot(values_of_n, theoretical_values, "--r", label="theoretical")
+    plt.legend(loc="best")
+    plt.title("Average-case complexity of Insertion Sort")
+    plt.xlabel("size of the list to sort (integer)")
+    plt.ylabel("average number of operations to perform insertion sort")
+    plt.savefig("complexity.pdf")
 
-"""
-    Plot the results.
-"""
-plt.xticks(range(0, n_max, step))
-plt.plot(values_of_n, theoretical_values, "--r", label="theoretical")
-plt.legend(loc="best")
-plt.title("Average-case complexity of Insertion Sort")
-plt.xlabel("size of the list to sort (integer)")
-plt.ylabel("average number of operations to perform insertion sort")
-plt.savefig("complexity.pdf")
+if __name__ == "__main__":
+    main()
