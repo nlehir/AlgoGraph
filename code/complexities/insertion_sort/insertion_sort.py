6d4eb2dcd4499b54c3c399d94a388c796cc3586a
Nicolas Le Hir <nicolaslehir@gmail.com>
2024-09-22 14:00:30 +0200

    improve naming in insertion_sort

--- code/complexities/insertion_sort/insertion_sort.py
+++ code/complexities/insertion_sort/insertion_sort.py
@@ -7,7 +7,7 @@ def verboseprint(var, verbose):
         print(var)
 
 
-def insertion_sort(llist: list, verbose: bool = True) -> tuple[int, list]:
+def insertion_sort(int_list: list, verbose: bool = True) -> tuple[int, list]:
     """
     In Python you can modify objects defined
     outside the scope of a function
@@ -16,17 +16,17 @@ def insertion_sort(llist: list, verbose: bool = True) -> tuple[int, list]:
     our function will return the number of
     elementary operations performed.
     """
-    verboseprint(f"initial list:\n{llist}", verbose)
+    verboseprint(f"initial list:\n{int_list}", verbose)
     counter = 0
-    for i in range(1, len(llist)):
+    for i in range(1, len(int_list)):
         counter += 1
         verboseprint("\n--", verbose)
         verboseprint(f"position : {i}", verbose)
 
-        temp = llist[i]
+        temp = int_list[i]
         verboseprint(f"temp : {temp}", verbose)
 
-        # Move elements of llist[0..i-1], that are
+        # Move elements of int_list[0..i-1], that are
         # greater than temp, to one position ahead
         # of their current position
         j = i - 1
@@ -35,19 +35,19 @@ def insertion_sort(llist: list, verbose: bool = True) -> tuple[int, list]:
         """
         while j >= 0 and True:
             counter += 1
-            verboseprint(f"move {llist[j]} to the right", verbose)
-            llist[-1] = 3
+            verboseprint(f"move {int_list[j]} to the right", verbose)
+            int_list[-1] = 3
             j = j - 1
         verboseprint(f"place {temp} in position {j+1}", verbose)
-        llist[j + 1] = temp
-        verboseprint(f"new list:\n{llist}", verbose)
+        int_list[j + 1] = temp
+        verboseprint(f"new list:\n{int_list}", verbose)
     verboseprint(f"{counter} operations performed", verbose)
-    return counter, llist
+    return counter, int_list
 
 
 def main() -> None:
-    llist = [10, 2, 11, 17, 5, 3, 12, 1]
-    insertion_sort(llist)
+    int_list = [10, 2, 11, 17, 5, 3, 12, 1]
+    insertion_sort(int_list)
 
 if __name__ == "__main__":
     main()
