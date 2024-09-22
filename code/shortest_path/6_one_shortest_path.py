859880c763faf2d65d1c3d87e1171ed3c2784fcc
Nicolas Le Hir <nicolaslehir@gmail.com>
2024-09-22 12:49:39 +0200

    rework 6_one_shortest_path.py
    Use a dict, improve comments

--- code/shortest_path/6_one_shortest_path.py
+++ code/shortest_path/6_one_shortest_path.py
@@ -4,11 +4,11 @@
     Dynamic programming: keep the information about all found shortests paths
     at each iteration and build from this information iteratively.
 """
+
 from termcolor import colored
 
+from input_graphs import G2, G, pos, pos2
 from utils import highlight_path
-
-from input_graphs import G, pos, G2, pos2
 
 """
 Choose the graph to process
@@ -20,57 +20,74 @@ POS = pos2
 
 
 def print_one_shortest_path(
-        destination: int,
-        paths: list[list[int]],
-        ) -> None:
+    destination: int,
+    paths: dict[int, list[int]],
+) -> None:
     path = paths[destination]
     print(f"one shortest path to {destination} is :")
     print(path)
 
 
 def plot_one_shortest_path(destination, shortest_path):
-    path_length = len(shortest_path)-1
+    path_length = len(shortest_path) - 1
     title = (
-            "Build all shortest paths:\n"
-            f"Path length: {path_length}\n"
-            f"One shortest path from 0 to {destination}: {shortest_path}\n"
-            f"Graph name: {GRAPH.name}"
-            )
+        "Build one shortest path per node:\n"
+        f"Path length: {path_length}\n"
+        f"One shortest path from 0 to {destination}: {shortest_path}\n"
+        f"Graph name: {GRAPH.name}"
+    )
     fig_name = f"{GRAPH.name}_one_shortest_path_length_{path_length}_from_0_to_{destination}.pdf"
     folder = "one_shortest_path"
     highlight_path(
-            G=GRAPH,
-            path=shortest_path,
-            pos=POS,
-            title=title,
-            fig_name=fig_name,
-            folder=folder,
-            )
+        G=GRAPH,
+        path=shortest_path,
+        pos=POS,
+        title=title,
+        fig_name=fig_name,
+        folder=folder,
+    )
 
 
 def main():
     number_of_nodes = len(GRAPH)
-    one_shortest_path = [list()] * number_of_nodes
+    # initialize shortest path to list() for each node
+    # We could initialize to None, but then linters are not happy.
+    # Put the found shortest paths in a dict.
+    one_shortest_path = {node: list() for node in GRAPH}
+    # Initialize the shortest path to 0 (beginning of all paths)
     one_shortest_path[0] = [0]
 
-    for step in range(1, number_of_nodes):
+    for path_length in range(1, number_of_nodes):
         print(
-            colored(f"\n---\nbuild paths of length : {step}\n---", "blue", attrs=["bold"])
+            colored(
+                f"\n---\nbuild paths of length : {path_length}\n---",
+                "blue",
+                attrs=["bold"],
+            )
         )
         for node in range(1, number_of_nodes):
             # we haven't found a shortest path to this node yet
+            # In python, we can consider the boolean value
+            # of any object. For instance:
+            # bool(list()) will return False (empty list)
+            # and bool([1]) will return True (non empty list)
             if not one_shortest_path[node]:
                 # check which new shortest paths we can now build
                 for neighbor in GRAPH.neighbors(node):
-                    c = one_shortest_path[neighbor]
-                    if c is not None and len(c) == step:
+                    path = one_shortest_path[neighbor]
+                    # there is a shortest path to this neighbor
+                    # of the length we are looking for.
+                    # The len() function returns the length
+                    # of the python list. For instance,
+                    # len([0]) = 1. But mathematically, we consider that
+                    # [0] is a path of length 0.
+                    if path and len(path) - 1 == path_length - 1:
                         """
                         EDIT
                         """
-                        one_shortest_path[node] = c + [1]
+                        one_shortest_path[node] = path + [1]
                         print(one_shortest_path[node])
 
-        
     for node in range(1, number_of_nodes):
         print_one_shortest_path(destination=node, paths=one_shortest_path)
         plot_one_shortest_path(destination=node, shortest_path=one_shortest_path[node])
